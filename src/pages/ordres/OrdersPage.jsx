242c6c4cabc7a14f287e0579cf43155a60e55162
shahan <shahanverse@gmail.com>
2025-09-11 01:10:29 +0530

    Separate into smaller components

--- src/pages/ordres/OrdersPage.jsx
+++ src/pages/ordres/OrdersPage.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 import dayjs from "dayjs";
 import { useState, useEffect, Fragment } from "react";
-import Header from "../components/Header";
-import { formatMoney } from "../utils/money";
+import Header from "../../components/Header";
+import { formatMoney } from "../../utils/money";
 import "./OrdersPage.css";
 function OrdersPage({ cart }) {
   const [orders, setOrders] = useState([]);
