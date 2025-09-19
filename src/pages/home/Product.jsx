3d5eeb34d378c6bd8040662858370fa5988d7706
shahan <shahanverse@gmail.com>
2025-09-20 00:06:28 +0530

    Create integration test for HomePage

--- src/pages/home/Product.jsx
+++ src/pages/home/Product.jsx
@@ -18,7 +18,8 @@ export function Product({ product, loadCart }) {
   };
 
   return (
-    <div className="product-container">
+    <div className="product-container"
+     data-testId ='product-container'>
       <div className="product-image-container">
         <img className="product-image" data-testId='product-image' src={product.image} />
       </div>
