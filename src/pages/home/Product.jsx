6f167ecd06a63842c05b9df91c1b5975e1b4eed7
shahan <shahanverse@gmail.com>
2025-09-19 22:22:44 +0530

    test clicking add to cart button

--- src/pages/home/Product.jsx
+++ src/pages/home/Product.jsx
@@ -64,6 +64,7 @@ export function Product({ product, loadCart }) {
       </div>
       <button
         className="add-to-cart-button button-primary"
+        data-testId='add-to-cart-button'
         onClick={addToCart}
       >
         Add to Cart
